9b6e5be55a8759ed593c7af2dcef5fb83203a302
DenisKipchirchirMaiyo <dameanmasha649@gmail.com>
2025-03-31 14:23:43 +0300

    event listener

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 function addingEventListener() {
+    const input = document.getElementById('button'); // Get the input element
+    
+    // Define the click alert function
+    function clickAlert() {
+        alert('I was clicked!');
+    }
+
+    // Add the event listener for the click event
+    input.addEventListener('click', clickAlert);
 }
+
+// Call the function to activate the event listener
+addingEventListener();
